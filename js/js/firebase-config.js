c62633939d1996d45c27ab5e44916f4b26e69891
YiChen-Hung916 <janehung0916@gmail.com>
2026-09-20 08:28:11 +0800

    Delete js/js/firebase-config.js

--- js/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBCbJV_WzkFDYNjQtKjMU6gBmlf99EZmkk",
-  authDomain: "personal-finance-for-abroad.firebaseapp.com",
-  projectId: "personal-finance-for-abroad",
-  storageBucket: "personal-finance-for-abroad.firebasestorage.app",
-  messagingSenderId: "335342836197",
-  appId: "1:335342836197:web:1a6ed934db0afff692f70c"
-};
